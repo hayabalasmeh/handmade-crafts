21c9bd7c4601e666dc9b0312a53d291f4bed0620
hayabalasmeh <hmbalasmeh@yahoo.com>
2021-03-28 15:37:31 +0300

    adding loops

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,28 @@
 
+
 alert("Hello To Handmade Crafts Made by Professional People around the World");
 
 prompt('Do you like Handmade crafts?','yes/no');
 
 
 
-var TypeOfCraft = prompt('Choose the type of the Craft you are intrested in Buying? 1-Textile crafts 2-Decorative Crafts 3-Fashion Crafts');
-var wrongchoice = ""
-if (TypeOfCraft === 'textile crafts') {
-    document.write('<img src="https://i.pinimg.com/originals/9b/a6/ec/9ba6eca428b28d4a3b88e1ff6e1b9eab.jpg">')
-    
-} else if (TypeOfCraft === 'decorative crafts') {
-    document.write('<img src="https://cdn.shopify.com/s/files/1/0047/4015/3437/products/image_ed37d48a-700f-4122-a5a6-340eb781ec3c_600x.jpg?v=1598633078"/>')
-} else if (TypeOfCraft === 'fashion crafts'){
-    document.write('<img src="https://cdn.shopify.com/s/files/1/1150/2764/files/Kalamkari_80a6be51-ee33-4c65-ba4f-fc272b62e017_1024x1024.jpg?v=1559559375"/>')
-} else { alert("Could you Please choose one the mentioned") }
+var typeOfCraft = prompt('Choose the type of the Craft you are intrested in Buying? 1-Textile crafts 2-Decorative Crafts 3-Fashion Crafts');
+
+while (typeOfCraft !== 'textile crafts' && typeOfCraft !== 'decorative crafts' && typeOfCraft !== 'fashion crafts'){
+    typeOfCraft = prompt('Could you please choose the type of the Craft you are intrested in Buying? 1-Textile crafts 2-Decorative Crafts 3-Fashion Crafts');
+}
+var image = '';
+var craftNumber = prompt('Choose number of the images for craft type you choosed');
+for (var num = 1; num <= craftNumber ; num ++ ) {
+    if (typeOfCraft === 'textile crafts'){
+        image = image +'<img src="images/textile.jpg"/>';
+        
+    } else if (typeOfCraft === 'decorative crafts') {
+        image = image +'<img src="images/decorative.jpg"/>';
+    } else if (typeOfCraft === 'fashion crafts'){
+        image = image +'<img src="images/fashion.jpg"/>';
+        
+}
+}
+document.write(image);
 
